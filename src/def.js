dbc669dc0d3700f9782424179e8e754e6aa9b6cb
Derpotron20 <matthew.f.a.braga@gmail.com>
2025-09-01 15:13:08 +1000

    All 6 moduels of Year 11 definitions are in place.

--- src/def.js
+++ src/def.js
@@ -3,13 +3,38 @@ fetch("src/components/defs/intro.html", { cache: "no-store" }) // No cacheing un
   .then(html => {
     document.getElementById("intro").innerHTML = html;
   });
+
+
 fetch("src/components/defs/cons_busi.html", { cache: "no-store" }) // No cacheing until navbar is complete to avoid lack of update.
   .then(res => res.text())
   .then(html => {
     document.getElementById("cons_busi").innerHTML = html;
   });
+
+
 fetch("src/components/defs/markets.html", { cache: "no-store" }) // No cacheing until navbar is complete to avoid lack of update.
   .then(res => res.text())
   .then(html => {
     document.getElementById("markets").innerHTML = html;
+  });
+
+
+fetch("src/components/defs/labour.html", { cache: "no-store" }) // No cacheing until navbar is complete to avoid lack of update.
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("labour").innerHTML = html;
+  });
+
+
+fetch("src/components/defs/finance.html", { cache: "no-store" }) // No cacheing until navbar is complete to avoid lack of update.
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("finance").innerHTML = html;
+  });
+
+
+fetch("src/components/defs/gov.html", { cache: "no-store" }) // No cacheing until navbar is complete to avoid lack of update.
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("gov").innerHTML = html;
   });
