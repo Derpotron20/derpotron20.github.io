1edc48d02eafe5af2ff520bd1dcaa50106dde915
Derpotron20 <matthew.f.a.braga@gmail.com>
2025-09-01 22:44:38 +1000

    Deleted caching of data for faster loading.

--- src/def.js
+++ src/def.js
@@ -1,46 +1,46 @@
-fetch("src/components/defs/current.html", { cache: "no-store" }) // No cacheing until navbar is complete to avoid lack of update.
+fetch("src/components/defs/current.html") // No cacheing until navbar is complete to avoid lack of update.
   .then(res => res.text())
   .then(html => {
     document.getElementById("current").innerHTML = html;
   });
 
   
-fetch("src/components/defs/intro.html", { cache: "no-store" }) // No cacheing until navbar is complete to avoid lack of update.
+fetch("src/components/defs/intro.html") // No cacheing until navbar is complete to avoid lack of update.
   .then(res => res.text())
   .then(html => {
     document.getElementById("intro").innerHTML = html;
   });
 
 
-fetch("src/components/defs/cons_busi.html", { cache: "no-store" }) // No cacheing until navbar is complete to avoid lack of update.
+fetch("src/components/defs/cons_busi.html") // No cacheing until navbar is complete to avoid lack of update.
   .then(res => res.text())
   .then(html => {
     document.getElementById("cons_busi").innerHTML = html;
   });
 
 
-fetch("src/components/defs/markets.html", { cache: "no-store" }) // No cacheing until navbar is complete to avoid lack of update.
+fetch("src/components/defs/markets.html") // No cacheing until navbar is complete to avoid lack of update.
   .then(res => res.text())
   .then(html => {
     document.getElementById("markets").innerHTML = html;
   });
 
 
-fetch("src/components/defs/labour.html", { cache: "no-store" }) // No cacheing until navbar is complete to avoid lack of update.
+fetch("src/components/defs/labour.html") // No cacheing until navbar is complete to avoid lack of update.
   .then(res => res.text())
   .then(html => {
     document.getElementById("labour").innerHTML = html;
   });
 
 
-fetch("src/components/defs/finance.html", { cache: "no-store" }) // No cacheing until navbar is complete to avoid lack of update.
+fetch("src/components/defs/finance.html") // No cacheing until navbar is complete to avoid lack of update.
   .then(res => res.text())
   .then(html => {
     document.getElementById("finance").innerHTML = html;
   });
 
 
-fetch("src/components/defs/gov.html", { cache: "no-store" }) // No cacheing until navbar is complete to avoid lack of update.
+fetch("src/components/defs/gov.html") // No cacheing until navbar is complete to avoid lack of update.
   .then(res => res.text())
   .then(html => {
     document.getElementById("gov").innerHTML = html;
--- src/navbar.js
+++ src/navbar.js
@@ -1,4 +1,4 @@
-fetch("src/components/navbar.html", { cache: "no-store" }) // No cacheing until navbar is complete to avoid lack of update.
+fetch("src/components/navbar.html")
   .then(res => res.text())
   .then(html => {
     document.getElementById("navbar").innerHTML = html;
